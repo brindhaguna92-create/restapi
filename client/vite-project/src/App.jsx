53f2c1a385ca9bca384baa5e5883a5c113ede0a2
Selvakumaran <selvakumarans@acinfotech.com>
2026-02-05 14:07:23 +0530

    datainsert

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,5 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import AddUser from './adduser/AddUser'
+import Users from './getuser/Users'
+
+function App() {
+  return (
+    <div>
+      <h1>welcome to front end</h1>
+      <AddUser/>
+      <Users/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+/*import { useState } from 'react'
+port reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -32,4 +50,4 @@ function App() {
   )
 }
 
-export default App
+export default App*/
---
+++ client/vite-project/src/getuser/Users.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+function Users() {
+    const[allusers,setAllUsers]=useState([])
+    useEffect(()=>{
+         async function show() {
+            await axios.get("http://localhost:8009/api/users").then((res)=>{
+setAllUsers(res.data)
+console.log(res.data)
+            }).catch((err)=>{
+                console.log(err.response.data)
+            })
+            
+         }
+         show()
+    },[])
+  return (
+    <div>
+      <h1>All user Details</h1>
+      <table border='2'>
+      {allusers.map((item,index)=>
+    <tr>
+    <td>{item.name}</td> <td>{item.email}</td> <td>{item.address}</td><br></br>
+    </tr>
+    
+)}
+</table>
+    </div>
+  )
+}
+
+export default Users
